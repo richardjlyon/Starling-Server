a5dc3b3eac03196853b94e25677705dd1b44d6e0
Richard <richlyon@mac.com>
2022-03-08 09:10:08 +0000

    add function to set system path at startup
    Running mypy from the project root requires import statements to be qualified from the root. But, out of the box, sys.path only includes the `src` folder in the project root. So qualifying import statements from the root breaks python. So this adds the project root to `sys.path`. Smells a bit odd - figure out how `sys.path` is set initially instead.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
+import sys
+from pathlib import Path
+
 import uvicorn
 
+
+def set_path():
+    """Add project root to system path for submodule searching."""
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    sys.path.insert(0, str(BASE_DIR))
+
+
 if __name__ == "__main__":
+    set_path()
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
