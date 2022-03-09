251632d6f48f6a8aa76e7da1ba1c8b10bbf41f57
Richard <richlyon@mac.com>
2022-03-09 13:41:24 +0000

    refactor code to add project path

--- src/main.py
+++ src/main.py
@@ -1,21 +1,16 @@
 import sys
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
+
 import uvicorn
 
-from src.db.edgedb.database import Database
-from src.server.controller import Controller
+from db.edgedb.database import Database
+from server.controller import Controller
 
 db = Database()
 controller = Controller(db=db)
 
-
-def set_path():
-    """Add project root to system path for submodule searching."""
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    sys.path.insert(0, str(BASE_DIR))
-
-
 if __name__ == "__main__":
-    set_path()
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
