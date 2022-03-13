133c0ac3fd569f43a89e5add2c09fddcda164b5d
Richard <richlyon@mac.com>
2022-03-13 05:50:51 +0000

    increase length of default period for transactions

--- starling_server/config.py
+++ starling_server/config.py
@@ -2,3 +2,4 @@ from config_path import ConfigPath
 
 config_path = ConfigPath("starling_server", "rjlyon.com", ".json")
 tokens_folder = config_path.saveFolderPath() / "tokens"
+default_interval_days = 14
