11e80056df123e1ff105cbbbd05c2907aaeaf2df
Richard <richlyon@mac.com>
2022-03-14 16:57:34 +0000

    add ConfigHelper (WIP)

---
+++ starling_server/server/config_helper.py
@@ -0,0 +1,47 @@
+# config_helper.py
+#
+# A class to manage server configuration
+import importlib
+
+# FIXME populate from ConfigHelper
+from starling_server.db.edgedb.database import Database
+from starling_server.providers.api_base import BaseAPI
+
+bank_classes = {"Starling": "starling", "SpareBanken": "sparebanken"}
+
+
+class ConfigHelper:
+    """
+    A class for managing configuration of the server e.g. add Banks and accounts
+    """
+
+    def __init__(self, db: Database):
+        self.db = db
+
+    async def initialise_bank(self, bank_name: str, token: str):
+        """
+        Initialise a Bank and associated accounts.
+
+        Using an api object initialised with the given bank, retrieves the accounts authorised for the token, and
+        inserts the bank and accounts in the database.
+
+        Args:
+            bank_name (str): The name of the bank
+            token (str): An authorisation token
+
+        Returns:
+            A list of Account objects
+
+        """
+        api_class = get_class_for_bank_name(bank_name)
+        api = api_class(bank_name=bank_name, auth_token=token)
+        accounts = await api.get_accounts()
+        print(accounts)
+
+
+def get_class_for_bank_name(bank_name) -> BaseAPI:
+    """Returns a class object computed from the bank_name"""
+    api_class = bank_classes.get(bank_name)
+    module = importlib.import_module(f"starling_server.providers.{api_class}.api_v2")
+    class_ = getattr(module, "APIV2")
+    return class_
---
+++ tests/server/test_config_helper.py
@@ -0,0 +1,16 @@
+import pytest
+
+from starling_server.server.config_helper import ConfigHelper
+from tests.conftest import select_accounts
+
+
+@pytest.mark.asyncio
+async def test_initialise_bank(empty_db, personal_auth_token):
+    # GIVEN a configuration object with an empty database
+    config = ConfigHelper(db=empty_db)
+
+    # WHEN I initialise a bank with a valid bank name and authorisation token
+    await config.initialise_bank("Starling", personal_auth_token)
+
+    # THEN the bank and account(s) are inserted in the database
+    print(select_accounts())
