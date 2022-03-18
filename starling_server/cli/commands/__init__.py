9ff902f80e00a2e5f1931680a01c55639b9a7fd4
Richard <richlyon@mac.com>
2022-03-18 08:55:32 +0000

    fix import bug

--- starling_server/cli/commands/__init__.py
+++ starling_server/cli/commands/__init__.py
@@ -1,4 +1,2 @@
-from starling_server.cli.commands.database.database import DatabaseCommand
 from .account import AccountCommand
-from .database import DatabaseCommand
 from .server import ServerCommand
