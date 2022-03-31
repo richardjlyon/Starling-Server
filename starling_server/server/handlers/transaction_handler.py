61d0dfb841e0fb902ec99a593f18d2c8d0cc3b85
Richard <richlyon@mac.com>
2022-03-31 13:17:20 +0100

    convert functions into class methods

--- starling_server/server/handlers/transaction_handler.py
+++ starling_server/server/handlers/transaction_handler.py
@@ -38,11 +38,9 @@ class TransactionHandler(Handler):
         if start_date is None:
             start_date = end_date - timedelta(days=cfg.default_interval_days)
 
-        # Fetch new transactions from the providers
-        for account in self.accounts:
-            new_transactions = await get_new_transactions(self.db, account)
-            insert_new_transactions(self.db, new_transactions)
-
+        # Fetch new transactions from the providers and insert them into the database
+        new_transactions = await self.get_new_transactions()
+        self.insert_transactions(new_transactions)
         transactions = self.db.select_transactions_between(start_date, end_date)
         # processed_transactions = process_new_transactions(self.db, new_transactions)
 
@@ -51,23 +49,41 @@ class TransactionHandler(Handler):
 
         return transactions
 
+    async def get_new_transactions(self) -> Optional[List[TransactionSchema]]:
+        """Get all transactions for each account since the last recorded transaction date and insert in the database."""
+        transactions = []
 
-async def get_new_transactions(
-    db: Database, account: Account
-) -> List[TransactionSchema]:
-    """Get all transactions for the account since the last recorded transaction date."""
-    latest_transaction_time = get_latest_transaction_time(db, account)
-    return await account.provider.get_transactions_between(
-        start_date=latest_transaction_time, end_date=datetime.now()
-    )
+        for account in self.accounts:
+            latest_transaction_time = self.get_latest_transaction_time(account)
+            new_transactions = await account.provider.get_transactions_between(
+                start_date=latest_transaction_time, end_date=datetime.now()
+            )
+            if len(new_transactions) > 0:
+                transactions.extend(new_transactions)
 
+        return transactions
 
-def insert_new_transactions(
-    db: Database, transactions: List[TransactionSchema]
-) -> None:
-    """Insert new transactions into the database."""
-    for transaction in transactions:
-        db.upsert_transaction(transaction)
+    def insert_transactions(self, transactions: List[TransactionSchema]) -> None:
+        """Insert transactions into the database."""
+        for transaction in transactions:
+            self.db.upsert_transaction(transaction)
+
+    def get_latest_transaction_time(self, account: Account) -> Optional[datetime]:
+        """Returns the time of the latest transaction for the specified account."""
+
+        transactions = self.db.select_transactions_for_account(account.schema.uuid)
+
+        if transactions is None:
+            # no transactions: compute from the default interval
+            transaction_time = datetime.now() - timedelta(
+                days=cfg.default_interval_days
+            )
+        else:
+            # transactions are sorted by time, so the last one is the latest
+            # add a millisecond to avoid retrieving it again
+            transaction_time = transactions[0].time + timedelta(milliseconds=1)
+
+        return transaction_time
 
 
 def process_new_transactions(
@@ -87,19 +103,3 @@ def process_new_transactions(
         pass  # TODO
 
     return transactions
-
-
-def get_latest_transaction_time(db: Database, account: Account) -> Optional[datetime]:
-    """Returns the time of the latest transaction for the specified account."""
-
-    transactions = db.select_transactions_for_account(account.schema.uuid)
-
-    if transactions is None:
-        # no transactions: compute from the default interval
-        transaction_time = datetime.now() - timedelta(days=cfg.default_interval_days)
-    else:
-        # transactions are sorted by time, so the last one is the latest
-        # add a millisecond to avoid retrieving it again
-        transaction_time = transactions[0].time + timedelta(milliseconds=1)
-
-    return transaction_time
